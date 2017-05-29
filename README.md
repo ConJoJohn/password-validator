af455dc5ee5266e7d1ce6090dd6d509efb5a9648
ConJoJohn <connor.johnson@dal.ca>
2017-05-29 05:54:43 -0300

    Created Android Studio project and added passwordValidator method and JUnit test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # password-validator
-Password validation tool for a CSCI 3130 assignment at Dalhousie University
+Password validation tool for a CSCI 3130 assignment at Dalhousie University.
+
+### Rules for Passwords
+* Cannot be "password" (case insensitive)
+* Must be at least 8 characters long
---
+++ app/src/main/java/ca/conjojohn/ass2/assignment02/MainActivity.java
@@ -0,0 +1,42 @@
+package ca.conjojohn.ass2.assignment02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static String passwordValidator(String password) {
+        if (password.equals("password")) {
+            return "Password cannot be \"password\"!";
+        } else if (password.length() < 8) {
+            return "Password must be at least 8 characters!";
+        } else {
+            return "This password is acceptable.";
+        }
+    }
+
+    /*
+    public void passwordValidator(View view) {
+        TextView passwordValidity = (TextView) findViewById(R.id.passwordValidity);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        if (password.equals("password")) {
+            passwordValidity.setText("Password cannot be \"password\"!");
+        } else if (password.length() < 8) {
+            passwordValidity.setText("Password must be at least 8 characters!");
+        } else {
+            passwordValidity.setText("This password is acceptable.");
+        }
+    }
+    */
+}
